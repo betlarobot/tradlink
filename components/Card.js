1d33ae5994c64733b2018679aa84aba29c553d66
betlarobot <mobitektechnology@gmail.com>
2026-04-28 18:09:32 +0530

    Delete components directory

--- components/Card.js
+++
@@ -1,8 +0,0 @@
-export default function Card({ title, value }) {
-  return (
-    <div className="bg-[#111827] p-5 rounded-xl shadow">
-      <p className="text-gray-400">{title}</p>
-      <h2 className="text-2xl font-bold mt-2">{value}</h2>
-    </div>
-  );
-}
